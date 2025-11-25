230c0e4f5d78ccaaab0eb22bbb1dcdabfd2c6c12
Shilton7 <shilton.sistemas@gmail.com>
2025-11-25 18:42:25 -0300

    Criando Model Usuario e Conexão DB

---
+++ backend/servico-aula-api/servico-aula-api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using servico_aula_api.Models;
+
+namespace servico_aula_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ backend/servico-aula-api/servico-aula-api/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace servico_aula_api.Models
+{
+    [Table("tb_usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+
+        [Column("data_cadastro")]
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+        public bool Ativo { get; set; } = false;
+    }
+}
