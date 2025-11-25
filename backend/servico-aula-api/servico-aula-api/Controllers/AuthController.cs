058046b779af6bbab193d43f42609ceb0cc0716f
Shilton7 <shilton.sistemas@gmail.com>
2025-11-25 18:43:03 -0300

    Criando Lógica de Autenticação

---
+++ backend/servico-aula-api/servico-aula-api/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using servico_aula_api.Data;
+using servico_aula_api.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace servico_aula_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/<AuthController>
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginRequest dados)
+        {
+            if (string.IsNullOrWhiteSpace(dados.Email) ||
+                string.IsNullOrWhiteSpace(dados.Senha))
+                return BadRequest("Email e senha são obrigatórios");
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(
+                u => u.Email == dados.Email &&
+                u.Senha == dados.Senha &&
+                u.Ativo);
+
+            if(usuario == null)
+            {
+                return Unauthorized("Email ou senha inválidos");
+            }
+
+            return Ok(new
+            {
+                usuario.Id,
+                usuario.Nome,
+                usuario.Email
+            });
+        }
+    }
+}
---
+++ backend/servico-aula-api/servico-aula-api/Models/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace servico_aula_api.Models
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = "";
+        public string Senha { get; set; } = "";
+
+    }
+}
